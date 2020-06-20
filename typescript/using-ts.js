861d8f83c6052fa8d7f7d5a352f5ced0c05c1e0d
fwcorp <gseongkyu79@gmail.com>
2020-06-20 20:18:35 +0900

    adding new code

---
+++ typescript/using-ts.js
@@ -0,0 +1,6 @@
+var a = 10;
+var b = 10;
+function app(test1, test2) {
+    return test1 + test2;
+}
+console.log(app(a, b));
---
+++ typescript/using-ts.ts
@@ -0,0 +1,9 @@
+var a = 10;
+var b = 10;
+var c = "hello"
+
+function app(test1: number, test2: number) {
+    return test1 + test2;
+}
+
+console.log(app(a, b));
