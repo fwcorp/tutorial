88ff8e3ed177e54fe70f9f89e506cbc949356967
fwcorp <gseongkyu79@gmail.com>
2020-06-22 09:02:21 +0900

    adding new code

---
+++ html/html5/html5.js
@@ -0,0 +1,10 @@
+var changeFlag = 0;
+function change(){
+    if(changeFlag==0){
+        $('#changer').text('다시 바꾸기');
+        changeFlag = 1;
+    }else{
+        $('#changer').text('체인져');
+        changeFlag = 0;
+    }
+}
