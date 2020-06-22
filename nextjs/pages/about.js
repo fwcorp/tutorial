730c343a3bfa738b8dd9fc70d1ad2acb3e5f281c
fwcorp <gseongkyu79@gmail.com>
2020-06-22 12:23:54 +0900

    adding new code

--- nextjs/pages/about.js
+++ nextjs/pages/about.js
@@ -1,11 +1,13 @@
 import Navbar from '../components/Navbar';
+import Layout from '../components/Layout';
 
 const About = () => (
-    <div>
-        <Navbar />
-        <h1>About Bitprice</h1>
-        <p>Bitprice is price indicator</p>
-    </div>
+    <Layout>
+        <div>
+            <h1>About Bitprice</h1>
+            <p>Bitprice is price indicator</p>
+        </div>
+    </Layout>
 );
 
 export default About;
--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -1,10 +1,23 @@
-import Navbar from '../components/Navbar';
+import Layout from '../components/Layout';
+import Fetch from 'isomorphic-unfetch';
+import Prices from '../components/Prices';
 
-const Index = () => (
-    <div>
-        <Navbar />
-        <h1>Welcome to bitprice</h1>
-    </div>
+const Index = (props) => (
+    <Layout>
+        <div>
+            <h1>Welcome to bitprice</h1>
+            <Prices bpi={props.bpi} />
+        </div>
+    </Layout>
 );
+
+Index.getInitialProps = async function () {
+    const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+    const data = await res.json();
+
+    return {
+        bpi: data.bpi
+    }
+}
 
 export default Index;
