1ccd4af814f8dfec42a54d7a2730d9b0be11b4b9
fwcorp <gseongkyu79@gmail.com>
2020-06-22 10:39:36 +0900

    adding new code

---
+++ nextjs/pages/about.js
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar';
+
+const About = () => (
+    <div>
+        <Navbar />
+        <h1>About Bitprice</h1>
+        <p>Bitprice is price indicator</p>
+    </div>
+);
+
+export default About;
